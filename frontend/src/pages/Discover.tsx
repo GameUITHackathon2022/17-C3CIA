c3d8a9b1c793f8992e39b81e6067671430df264a
truong9c2208 <truong9c2208@gmail.com>
2022-11-27 02:02:16 +0700

    update discover

--- frontend/src/pages/Discover.tsx
+++ frontend/src/pages/Discover.tsx
@@ -1,28 +1,101 @@
 import React, { Component } from 'react';
-import { Container, styled, Typography } from '@mui/material';
+import { Container, styled, Typography, Checkbox, Box, IconButton } from '@mui/material';
+import { ListAlt } from '@mui/icons-material';
+import Data from '../data/data.json'
+
+interface IType {
+    type: string;
+    data: IData[];
+}
+
+interface IData {
+    name: string;
+    section: string;
+    benefit: string[];
+    nutrition: Object;
+    price: Object;
+    view: number;
+}
+
+let _filter = ["Thịt", "Rau củ", "Hoa quả"];
 
 const StyledDiscoverContainer = styled(Container)(
     ({ theme }) => ({
         display: "flex",
-        justifyContent: "center",
         alignItems: "center",
-        textAlign: "center",
-        height: "100%"
+        width: "100%"
     })
 );
 
+const MenuFilter = styled(Box)(
+    ({ theme }) => ({
+        display: "flex",
+        justifyContent: "center",
+        position: "relative",
+        boxShadow: "0 0 .5px .5px black"
+    })
+)
+
 export default class Discover extends Component {
+    state = {
+        filter: new Array(_filter.length).fill(true),
+        menu: false
+    }
+
+    handleChange = (event: any, index: number) => {
+        const newFilter = [...this.state.filter];
+        newFilter[index] = event.target.checked;
+        this.setState({ filter: newFilter });
+    }
+
     render() {
         return (
             <StyledDiscoverContainer>
                 <div>
-                    <Typography variant='h5' component='h5'>
-                        404: NOT FOUND
-                    </Typography><br />
-                    <Typography variant='body1'>
-                        Khu vực bạn đang tìm không tồn tại, có thể khu vực này đã bị xóa.
-                    </Typography>
+                    <IconButton onClick={() => this.setState({ menu: !this.state.menu })}>
+                        <ListAlt />
+                    </IconButton>
                 </div>
+                <div>
+                    <MenuFilter
+                        left={this.state.menu ? "unset" : "-850px"}
+                    >
+                        {_filter.map((e, i) => (
+                            <Box
+                                key={e}
+                                sx={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    flexWrap: "wrap",
+                                    margin: "0 5px"
+                                }}
+                            >
+                                <Checkbox
+                                    checked={this.state.filter[i]}
+                                    onChange={(evt) => this.handleChange(evt, i)}
+                                    size="small"
+                                    sx={{
+                                        p: "3px"
+                                    }}
+                                />
+                                <Typography
+                                    variant="body1"
+                                    component="p"
+                                    sx={{
+                                        fontSize: "15px"
+                                    }}
+                                >
+                                    {e}
+                                </Typography>
+                            </Box>
+                        ))}
+                    </MenuFilter>
+                </div>
+                {Data.map((e: IType) => (
+                    <div>
+
+                    </div>
+                ))}
             </StyledDiscoverContainer>
         )
     }
