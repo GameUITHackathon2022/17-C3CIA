c7bbe416c62b08d1d5349c875cc95d2b492a8f45
BadAimWeeb <badaimweeb@protonmail.com>
2022-11-27 00:12:08 +0700

    connect tRPC to server?

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+const API_SERVER = "http://localhost:3000/api";
+
 import Header from './components/Header';
 import BottomNav from './components/BottomNav';
 
@@ -9,22 +11,43 @@ import Account from './pages/Account';
 import Error from './pages/Error';
 import Discover from './pages/Discover';
 
+// API
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { httpBatchLink } from '@trpc/client';
+import { trpc } from './trpc';
+import { useState } from 'react';
+
 export default function App() {
+    const [queryClient] = useState(() => new QueryClient());
+    const [trpcClient] = useState(() =>
+        trpc.createClient({
+            links: [
+                httpBatchLink({
+                    url: API_SERVER
+                }),
+            ],
+        }),
+    );
+
     return (
         <div className="app">
-            <Router>
-                <Header />
-                <div className="page">
-                    <Routes>
-                        <Route index element={<Home />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route path="/account" element={<Account />} />
-                        <Route path="/discover" element={<Discover />}></Route>
-                        <Route path="*" element={<Error />} />
-                    </Routes>
-                </div>
-                <BottomNav />
-            </Router>
+            <trpc.Provider client={trpcClient} queryClient={queryClient}>
+                <QueryClientProvider client={queryClient}>
+                    <Router>
+                        <Header />
+                        <div className="page">
+                            <Routes>
+                                <Route index element={<Home />} />
+                                <Route path="/home" element={<Home />} />
+                                <Route path="/account" element={<Account />} />
+                                <Route path="/discover" element={<Discover />}></Route>
+                                <Route path="*" element={<Error />} />
+                            </Routes>
+                        </div>
+                        <BottomNav />
+                    </Router>
+                </QueryClientProvider>
+            </trpc.Provider>
         </div>
     )
 }
